b91caf739e288506768f1ef7578845d1bdd88611
samuel-2004 <105411500+samuel-2004@users.noreply.github.com>
2023-04-28 21:27:01 +0800

    added question descriptions

--- main.java
+++ main.java
@@ -2,6 +2,33 @@
  * 
  */
 public class Main {
-
-
+  /**
+  * Question 1. Write a method that, given a pair of pages, 
+  * returns the minimum number of links you must follow to get from the first page to the second. 
+  * See (https://en.wikipedia.org/wiki/Shortest_path_problem) for more information.
+  */
+  /**
+  * 
+  */
+  
+  /**
+  * Question 2. Write a method that finds a Hamiltonian path in a Wikipedia page graph. 
+  * A Hamiltonian path is any path in some graph that visits every vertex exactly once. 
+  * This method will never be called for graphs with more than 20 pages. 
+  * See (https://en.wikipedia.org/wiki/Hamiltonian_path) for more information about Hamiltonian paths.
+  */
+  
+  /**
+  * Question 3. Write a method that finds every ‘strongly connected component’ of pages. 
+  * A strongly connected component is a set of vertices such that there is a path 
+  * between every ordered pair of vertices in the strongly connected component. 
+  * See (https://en.wikipedia.org/wiki/Strongly_connected_component) for more information about strongly connected components.
+  */
+  
+  /**
+  * Question 4. Write a method that finds all the centers of the Wikipedia page graph. 
+  * A vertex is considered to be the center of a graph if the maximum shortest 
+  * path from that vertex to any other vertex is the minimum possible. 
+  * See (https://en.wikipedia.org/wiki/Graph_center) for more information about ‘graph center’.
+  */
 }
