3052f0d2f4434c7e1bf45e04f2da313ccb4d176f
samuel-2004 <105411500+samuel-2004@users.noreply.github.com>
2023-04-28 21:50:54 +0800

    added method descriptors and summarys

--- main.java
+++ main.java
@@ -8,8 +8,16 @@ public class Main {
   * See (https://en.wikipedia.org/wiki/Shortest_path_problem) for more information.
   */
   /**
+  * A really good description of the function
   * 
+  * @param pageStart    The page to start searching from
+  * @param pageEnd      The page to search for
+  * 
+  * @return the minimum number of links
   */
+  public int getMinimumLinks(String pageStart, String pageEnd) {
+    
+  }
   
   /**
   * Question 2. Write a method that finds a Hamiltonian path in a Wikipedia page graph. 
@@ -17,6 +25,17 @@ public class Main {
   * This method will never be called for graphs with more than 20 pages. 
   * See (https://en.wikipedia.org/wiki/Hamiltonian_path) for more information about Hamiltonian paths.
   */
+  /**
+  * A really good description of the function
+  * 
+  * @param g    The graph representing the wikipedia pages
+  * 
+  * @return an array listing the distance to each vertex in the single source shortest path problem, 
+  *         or -1 if the vertex is not reachable from the source.
+  */
+  public int[] getHamiltonianPath(Graph g) {
+    // look into https://teaching.csse.uwa.edu.au/units/CITS2200/Resources/CITS2200Doc/CITS2200/Path.html
+  }
   
   /**
   * Question 3. Write a method that finds every ‘strongly connected component’ of pages. 
@@ -24,6 +43,16 @@ public class Main {
   * between every ordered pair of vertices in the strongly connected component. 
   * See (https://en.wikipedia.org/wiki/Strongly_connected_component) for more information about strongly connected components.
   */
+  /**
+  * A really good description of the function
+  * 
+  * @param g    The graph representing the wikipedia pages
+  * 
+  * @return i am unsure what it returns
+  */
+  public unknown_type getStrongConnections(Graph g) {
+    
+  }
   
   /**
   * Question 4. Write a method that finds all the centers of the Wikipedia page graph. 
@@ -31,4 +60,14 @@ public class Main {
   * path from that vertex to any other vertex is the minimum possible. 
   * See (https://en.wikipedia.org/wiki/Graph_center) for more information about ‘graph center’.
   */
+  /**
+  * A really good description of the function
+  * 
+  * @param g    The graph representing the wikipedia pages
+  * 
+  * @return i am unsure what it returns
+  */
+  public unknown_type getCentres(Graph g) {
+    
+  }
 }
